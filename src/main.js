b97fd6bd540abdaa4bcdc81fa4a55fbf9a2a197d
Fabián Adrés Salazar <fasj90@gmail.com>
2019-10-28 19:00:22 -0500

    Vue Router - Instalar Vue Router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 
 import EventBus from './plugins/event-bus';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
 Vue.use(EventBus);
 
 new Vue({
